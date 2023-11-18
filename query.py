611a9681be61fcaebf78336c7d95970f36c078d6
beepo34 <usmiaileen@gmail.com>
2023-11-17 16:49:28 -0800

    Initial commit of query.py

---
+++ query.py
@@ -0,0 +1,20 @@
+def query(query: str):
+    # TODO: boolean retrieval, AND only
+    terms = query.split()
+    
+    return []
+
+
+
+if __name__ == '__main__':
+    print("Search Engine Start.")
+
+    while True:
+        user_input = input("Enter a query (or 'exit' to quit): ")
+
+        if user_input.lower() == 'exit':
+            print("Exiting the Search Engine. Goodbye!")
+            break
+
+        results = query(user_input)
+        # TODO: show results
