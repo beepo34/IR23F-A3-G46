c59402308fd76c2ee0a4e8fcee1de8cd6b8ee2ab
Teresa Liang <teresaliang2003@gmail.com>
2023-11-30 01:48:21 -0800

    catch any errors in the query (e.g. misspelled words)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import time
 from query import _ranked_search
 from index import main, Posting
+from spellchecker import SpellChecker
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -22,17 +23,36 @@ def search_query():
     global index_index, id_map
     
     if request.method == "POST":
-        K = 15
-        start_time = time.time_ns()
-        query = request.form.get("query")
-        results_from_query = _ranked_search(query, index_index, id_map)
-        end_time = time.time_ns()
-        first_k_results = results_from_query[:K]
-        search_time = (end_time - start_time) / 10**6
-        
-        return render_template("results.html", query=query, num_results=len(results_from_query), first_k_results=first_k_results, search_time=search_time, K=K)
+        try:
+            K = 15
+            start_time = time.time_ns()
+            query = request.form.get("query")
+            results_from_query = _ranked_search(query, index_index, id_map)
+            end_time = time.time_ns()
+            first_k_results = results_from_query[:K]
+            search_time = (end_time - start_time) / 10**6
+            
+            return render_template("results.html", query=query, num_results=len(results_from_query), first_k_results=first_k_results, search_time=search_time, K=K)
+        except Exception as e:
+            print(e)
+            # check if user spelled any words wrong
+            query = request.form.get("query")
+            spell = SpellChecker()
+            misspelled = spell.unknown(query.split())
+            
+            if len(misspelled) == 0:
+                return render_template("index.html", error=True, errorMsg="Invalid query. Please try again.")
+
+            try:
+                for word in misspelled:
+                    # Get the one `most likely` answer
+                    query = query.replace(word, spell.correction(word))
+
+                return render_template("index.html", error=True, errorMsg=f"Invalid query. Did you mean: {query}?")
+            except Exception as e:
+                return render_template("index.html", error=True, errorMsg="Invalid query. Please try again.")
     else:
-        return render_template("index.html")
+        return render_template("index.html", error=False, errorMsg="")
 
 if __name__ == "__main__":
     app.run(debug=True)
